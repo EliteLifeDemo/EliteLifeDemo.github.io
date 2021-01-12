b359bdfb976ff981e7adcf28169675c2c43533c5
c9a2334 <c9a2334@gmail.com>
2021-01-12 16:29:28 +0300

    wip

--- js/main.js
+++ js/main.js
@@ -3,7 +3,13 @@ document.addEventListener('DOMContentLoaded',function () {
 });
 
 function pageReady() {
+    polyfills();
     initializeMainBanner();
+}
+function polyfills() {
+    if (window.NodeList && !NodeList.prototype.forEach) {
+        NodeList.prototype.forEach = Array.prototype.forEach;
+    }
 }
 function initializeMainBanner(){
     let sliderBox = document.querySelector('.main-page-banner .splide');
@@ -15,10 +21,25 @@ function initializeMainBanner(){
         arrows: false,
         cover: true,
         perPage: 1,
-        heightRatio: 0.5624,
+        heightRatio: 0.592,
     });
+    let reset = true;
     slider.on( 'autoplay:playing', function (rate) {
-        console.log(rate); // 0-1
+        let activeDot = sliderBox.querySelector('.splide__pagination__page.is-active');
+        let width = activeDot.width;
+        let parent = activeDot.parentElement;
+        let index = slider.index;
+        if(index === 0 && reset){
+            reset = false;
+            let dots = sliderBox.querySelector('.splide__pagination__page');
+            dots.forEach(function (dot) {
+                dot.parentElement.style.paddingRight = width + "px";
+            })
+        }
+        if(index !== 1){
+            reset = true;
+        }
+        parent.style.paddingRight = ((1 - rate) * width) + 'px';
     });
     slider.mount();
 }
