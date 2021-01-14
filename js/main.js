1b113a481eb6a68b82a20b05e6ad120b6771d329
c9a2334 <c9a2334@gmail.com>
2021-01-14 12:56:26 +0300

    wip

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
-document.addEventListener('DOMContentLoaded',function () {
+document.addEventListener('DOMContentLoaded', function () {
     pageReady();
 });
 
 function pageReady() {
     polyfills();
     initializeMainBanner();
+    initializeFLatSlider();
 }
+
 function polyfills() {
     if (window.NodeList && !NodeList.prototype.forEach) {
         NodeList.prototype.forEach = Array.prototype.forEach;
     }
 }
-function initializeMainBanner(){
+
+function initializeMainBanner() {
     let sliderBox = document.querySelector('.main-page-banner .splide');
     let slider = new Splide(sliderBox, {
-        type   : 'loop',
+        type: 'loop',
         autoplay: true,
         interval: 10000,
         lazyLoad: true,
@@ -25,13 +28,13 @@ function initializeMainBanner(){
         pauseOnHover: false,
         pauseOnFocus: false,
     });
-    slider.on( 'moved', function (newIndex) {
+    slider.on('moved', function (newIndex) {
         normalizePaginationButtons(sliderBox, newIndex);
     });
-    slider.on( 'dragged', function (newIndex) {
+    slider.on('dragged', function (newIndex) {
         normalizePaginationButtons(sliderBox, newIndex);
     });
-    slider.on( 'autoplay:playing', function (rate) {
+    slider.on('autoplay:playing', function (rate) {
         let activeDot = sliderBox.querySelector('.splide__pagination__page.is-active');
         let width = activeDot.offsetWidth;
         let parent = activeDot.parentElement;
@@ -44,13 +47,35 @@ function initializeMainBanner(){
 
 function normalizePaginationButtons(sliderBox, index) {
     let dots = sliderBox.querySelectorAll('.splide__pagination__page');
-    for (let i = 0; i < index; i++){
+    for (let i = 0; i < index; i++) {
         dots[i].parentElement.style.paddingRight = "0px";
         dots[i].classList.add('-full');
     }
-    for (let i = index; i < dots.length; i++){
+    for (let i = index; i < dots.length; i++) {
         let width = dots[i].offsetWidth;
         dots[i].parentElement.style.paddingRight = width + "px";
         dots[i].classList.remove('-full');
     }
+}
+
+function initializeFLatSlider() {
+    var secondarySlider = new Splide('.flat-preview-small', {
+        rewind: true,
+        fixedWidth: 246,
+        isNavigation: true,
+        gap: 3,
+        focus: 'center',
+        arrows: false,
+        pagination: false,
+    }).mount();
+
+// Create the main slider.
+    var primarySlider = new Splide('.flat-preview-big', {
+        type: 'fade',
+        pagination: false,
+        arrows: true,
+    });
+
+// Set the thumbnails slider as a sync target and then call mount.
+    primarySlider.sync(secondarySlider).mount();
 }
